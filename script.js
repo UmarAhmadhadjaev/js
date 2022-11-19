b0f27efc753d1630aafaf35052f78d1543c0e6a5
UmarAhmadxadjaev <azizhonahmadhadjaev@gmail.com>
2022-11-19 16:30:07 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let son = Math.floor(Math.random() * 10)+1;
+// let input = prompt("Son o'yladim. (1..10) Topib ko'ring.");
+// console.log(
+//   `Men ${son} sonini o'yladim, Siz ${input} sonini kiritdingiz.`
+// );\
+
+// let son = Math.floor(Math.random() * 10)+1;
+// let input = + prompt("Son o'yladim.")
+// console.log(son);
+// if (son === input) {
+//   document.write("to'g'ri");
+// } else if (input > son) {
+//   document.write("Siz katta son kiritdingiz.");
+// } else if (input < son){
+//   document.write("Siz kichkina son kiritdingiz!");
+// } else{
+//   document.write("Iltimos son kiriting!")
+// }
